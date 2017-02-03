b5e15072c134f1dbd0f92206601c89a6acf7c783
Zander Work <zander@zanderwork.com>
2017-02-03 07:22:09 -0800

    added twitch chat message parsing

--- src/com/zanderwork/twitchbot/SocketReader.java
+++ src/com/zanderwork/twitchbot/SocketReader.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.HashMap;
 
 /**
  * Created by Zander Work on 2/1/2017.
@@ -22,20 +23,61 @@ public class SocketReader implements Runnable {
 		reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 
 		myThread = new Thread(this);
+		myThread.setName("SocketReader");
 	}
 
 	public boolean isRunning() {
 		return running;
 	}
 
+	public HashMap<String, String> parseIRC(String message) {
+		//example incoming message:
+		//:memebot42!memebot42@memebot42.tmi.twitch.tv PRIVMSG #techgod52 :test
+		HashMap<String, String> messageParts = new HashMap<>();
+		int numColons = 0;
+		String user = null;
+		String chatMessage = null;
+		if (!message.contains("PRIVMSG")) {
+			//not a chat message, return
+			messageParts.put("error", "irc message not twitch chat message");
+			return messageParts;
+		}
+		for (int i = 0; i < message.length(); i++) {
+			if (message.charAt(i) == ':') {
+				numColons++;
+			}
+		}
+		String[] colon = message.split(":");
+		user = colon[1].split("!")[0];
+		chatMessage = colon[2];
+		if (numColons > 2) {
+			//the chat message had a colon(s) in it, add them and the message back in
+			for (int i = 3; i <= numColons; i++) {
+				chatMessage += ":";
+				chatMessage += colon[i];
+			}
+		}
+		messageParts.put("user", user);
+		messageParts.put("message", chatMessage);
+		return messageParts;
+	}
+
 	@Override
 	public void run() {
 		running = true;
 		String message;
+		HashMap<String, String> parsedMessage;
 		while (running) {
 			try {
 				message = reader.readLine();
-				System.out.println(message);
+				parsedMessage = parseIRC(message);
+				if (!parsedMessage.containsKey("error")) {
+					//we parsed a valid twitch chat message
+					Bot.log("CHAT_IN:" + ConfigLoader.getConfig().get("channel"), String.format("%s: %s", parsedMessage.get("user"), parsedMessage.get("message")));
+				} else {
+					//we attempted to parse a non-twitch chat message
+					Bot.log("IRC_IN", message);
+				}
 				if (message.contentEquals("PING :tmi.twitch.tv")) {
 					//avoid getting auto-disconnected
 					Bot.getWriter().sendIRCMessage("PONG :tmi.twitch.tv");
